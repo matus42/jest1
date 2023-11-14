ebe2d2d3e378d7a08056179b8087bcbe655fbb8f
matus42 <134312272+matus42@users.noreply.github.com>
2023-11-14 22:04:11 +0000

    Testing DOM

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,26 +1,26 @@
-const addition = require("../calc");
+// const addition = require("../calc");
 
-describe("Calculator", () => {
-    describe("Addition function", () => {
-        test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42);
-        });
-        test("should return 73 for 42 + 31", () => {
-            expect(addition(42, 31)).toBe(73);
-        });
-        test("should return 'Invalid input' when a string is provided", () => {
-            expect(addition("20", 22)).toBe('Invalid input');
-            expect(addition(20, "22")).toBe('Invalid input');
-            expect(addition("20", "22")).toBe('Invalid input');
-        });
-    });
-    describe("Subtraction function", () => {
+// describe("Calculator", () => {
+//     describe("Addition function", () => {
+//         test("should return 42 for 20 + 22", () => {
+//             expect(addition(20, 22)).toBe(42);
+//         });
+//         test("should return 73 for 42 + 31", () => {
+//             expect(addition(42, 31)).toBe(73);
+//         });
+//         test("should return 'Invalid input' when a string is provided", () => {
+//             expect(addition("20", 22)).toBe('Invalid input');
+//             expect(addition(20, "22")).toBe('Invalid input');
+//             expect(addition("20", "22")).toBe('Invalid input');
+//         });
+//     });
+//     describe("Subtraction function", () => {
 
-    });
-    describe("Multiply function", () => {
+//     });
+//     describe("Multiply function", () => {
 
-    });
-    describe("Division function", () => {
+//     });
+//     describe("Division function", () => {
 
-    });
-})
+//     });
+// })
